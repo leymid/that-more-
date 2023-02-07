a7746761e6bdfbb4ba36ec1ef9765e483d151f1a
leymid <124685397+leymid@users.noreply.github.com>
2023-02-08 00:37:38 +0500

    Add files via upload

---
+++ that more.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cout << "A nechiga barobarligini yozing:";
+	cin >> a;
+
+	cout << "B nechiga barobarligini yozing:";
+	cin >> b;
+
+	cout << "C nechiga barobarligini yozing:";
+	cin >> c;
+
+	cout << "D nechiga barobarligini yozing:";
+	cin >> d;
+
+	if (a + b > c + d) cout << "Birinchi sonlar ikinchi sonlardan kotta.";
+
+	if (a + b < c + d) cout << "Ikinchi sonlar birinchiga nisbatan kotta.";
+	return 0;
+
+}
